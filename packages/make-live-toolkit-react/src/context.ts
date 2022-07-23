4f40b914f6f3e9160049da6fccddc691b328e0c2
Chris Shepherd <chris@chrisshepherd.me>
2022-07-23 15:10:21 +0100

    Add setContainer to Make Live context

--- packages/make-live-toolkit-react/src/context.ts
+++ packages/make-live-toolkit-react/src/context.ts
@@ -1,12 +1,17 @@
 import { Instance } from "@make-live/toolkit";
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export type MakeLiveContextType = {
   instance?: Instance;
+  setContainer: (container: HTMLDivElement) => void;
 };
 
 const MakeLiveContext = createContext<MakeLiveContextType>({
   instance: undefined,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  setContainer: () => {},
 });
+
+export const useMakeLiveContext = () => useContext(MakeLiveContext);
 
 export default MakeLiveContext;
