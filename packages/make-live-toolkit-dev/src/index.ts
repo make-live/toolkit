c53fcc77952985d5c7032ce7ed7073b4c8fcd1b9
Chris Shepherd <chris@chrisshepherd.me>
2022-07-24 23:22:15 +0100

    Output localhost local network IP at startup

--- packages/make-live-toolkit-dev/src/index.ts
+++ packages/make-live-toolkit-dev/src/index.ts
@@ -1,6 +1,7 @@
 import { createApp } from "./app";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import { networkInterfaces } from "os";
 
 const args = yargs(hideBin(process.argv))
   .scriptName("make-live-dev")
@@ -20,7 +21,15 @@ const args = yargs(hideBin(process.argv))
   .help()
   .parseSync();
 
+const address =
+  Object.values(networkInterfaces())
+    .flat()
+    .find((ip) => String(ip?.family).includes("4") && !ip?.internal)?.address ??
+  "localhost";
+
 const app = createApp(args["player-url"]);
 app.listen(args["port"], () => {
-  console.log(`Listening on http://localhost:${args.port}`);
+  console.log(
+    `Listening on http://localhost:${args.port} (http://${address}:${args.port})`,
+  );
 });
