635fa726117f3a133fc00aceec39c6b0f1623402
Chris Shepherd <chris@chrisshepherd.me>
2022-07-24 21:22:55 +0100

    Add InteractionCommand

--- packages/make-live-toolkit/src/commands.test.ts
+++ packages/make-live-toolkit/src/commands.test.ts
@@ -1,11 +1,22 @@
 import { describe, expect, it } from "@jest/globals";
-import { ConsoleCommand, isValidCommand } from "./commands";
+import { ConsoleCommand, InteractionCommand, isValidCommand } from "./commands";
 
 describe("isValidCommand", () => {
   it("returns `true` for `CONSOLE_COMMAND` command", () => {
     const command: ConsoleCommand = {
       data: "stat fps",
       type: "CONSOLE_COMMAND",
+    };
+
+    const result = isValidCommand(command);
+
+    expect(result).toBe(true);
+  });
+
+  it("returns `true` for `INTERACTION_COMMAND` command", () => {
+    const command: InteractionCommand = {
+      data: "stat fps",
+      type: "INTERACTION_COMMAND",
     };
 
     const result = isValidCommand(command);
--- packages/make-live-toolkit/src/commands.ts
+++ packages/make-live-toolkit/src/commands.ts
@@ -1,16 +1,23 @@
 import { z } from "zod";
 
 export const consoleCommand = z.object({
-  data: z.string(),
+  data: z.string().min(1),
   type: z.literal("CONSOLE_COMMAND"),
 });
 
 export type ConsoleCommand = z.infer<typeof consoleCommand>;
 
+export const interactionCommand = z.object({
+  data: z.union([z.string().min(1), z.object({}).passthrough()]),
+  type: z.literal("INTERACTION_COMMAND"),
+});
+
+export type InteractionCommand = z.infer<typeof interactionCommand>;
+
 export type Command = ConsoleCommand;
 
 export const isValidCommand = (data: unknown): data is Command => {
-  const foundSchema = [consoleCommand].find((schema) => {
+  const foundSchema = [consoleCommand, interactionCommand].find((schema) => {
     const result = schema.safeParse(data);
 
     return result.success;
