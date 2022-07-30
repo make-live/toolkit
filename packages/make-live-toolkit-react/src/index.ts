aaa412b81f64dfe7b44ef0f9f29b49e2d433296c
Chris Shepherd <chris@chrisshepherd.me>
2022-07-30 15:34:09 +0100

    Add export of MakeLiveContext

--- packages/make-live-toolkit-react/src/index.ts
+++ packages/make-live-toolkit-react/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./context";
+export { default as MakeLiveContext } from "./context";
 export { default as MakeLiveProvider } from "./provider";
 export { default as useEvents } from "./use-events";
 export { default as useInstance } from "./use-instance";
